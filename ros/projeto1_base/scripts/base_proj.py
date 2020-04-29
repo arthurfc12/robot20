57b715a60a6690c0aae17f02aebb31ae2d5802c5
arthurfc12 <arthur0912fc@gmail.com>
2020-04-28 21:58:50 -0300

    começando conceito c

--- ros/projeto1_base/scripts/base_proj.py
+++ ros/projeto1_base/scripts/base_proj.py
@@ -19,8 +19,18 @@ import tf2_ros
 from geometry_msgs.msg import Twist, Vector3, Pose, Vector3Stamped
 from ar_track_alvar_msgs.msg import AlvarMarker, AlvarMarkers
 from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Image
 from std_msgs.msg import Header
-
+from numpy import linalg
+from tf import transformations
+from tf import TransformerROS
+import tf2_ros
+import math
+from geometry_msgs.msg import Twist, Vector3, Pose, Vector3Stamped
+from ar_track_alvar_msgs.msg import AlvarMarker, AlvarMarkers
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Image
+from std_msgs.msg import Header
 
 import visao_module
 
@@ -31,6 +41,11 @@ cv_image = None
 media = []
 centro = []
 atraso = 1.5E9 # 1 segundo e meio. Em nanossegundos
+
+white_1_hsv = np.array([0, 0, 78], dtype=np.uint8)
+white1 = np.array([200, 200, 200], dtype=np.uint8)
+white_2_hsv = np.array([0, 0, 100], dtype=np.uint8)
+white2 = np.array([255, 255, 255], dtype=np.uint8)
 
 
 area = 0.0 # Variavel com a area do maior contorno
@@ -52,7 +67,6 @@ frame = "camera_link"
 tfl = 0
 
 tf_buffer = tf2_ros.Buffer()
-
 
 def recebe(msg):
 	global x # O global impede a recriacao de uma variavel local, para podermos usar o x global ja'  declarado
@@ -100,7 +114,9 @@ def roda_todo_frame(imagem):
     global cv_image
     global media
     global centro
+
     global resultados
+
 
     now = rospy.get_rostime()
     imgtime = imagem.header.stamp
@@ -112,10 +128,10 @@ def roda_todo_frame(imagem):
         return 
     try:
         antes = time.clock()
-        temp_image = bridge.compressed_imgmsg_to_cv2(imagem, "bgr8")
+        cv_image = bridge.compressed_imgmsg_to_cv2(imagem, "bgr8")
         # Note que os resultados já são guardados automaticamente na variável
         # chamada resultados
-        centro, saida_net, resultados =  visao_module.processa(temp_image)        
+        centro, imagem, resultados =  visao_module.processa(cv_image)        
         for r in resultados:
             # print(r) - print feito para documentar e entender
             # o resultado            
@@ -123,7 +139,7 @@ def roda_todo_frame(imagem):
 
         depois = time.clock()
         # Desnecessário - Hough e MobileNet já abrem janelas
-        cv_image = saida_net.copy()
+        #cv2.imshow("Camera", cv_image)
     except CvBridgeError as e:
         print('ex', e)
     
@@ -151,17 +167,123 @@ if __name__=="__main__":
         # vel = Twist(Vector3(0,0,0), Vector3(0,0,math.pi/10.0))
         
         while not rospy.is_shutdown():
-            for r in resultados:
-                print(r)
-            #velocidade_saida.publish(vel)
-
             if cv_image is not None:
-                # Note que o imshow precisa ficar *ou* no codigo de tratamento de eventos *ou* no thread principal, não em ambos
-                cv2.imshow("cv_image no loop principal", cv_image)
-                cv2.waitKey(1)
+
+                mask_white = cv2.inRange(cv_image, white1, white2)
+
+                blur = cv2.GaussianBlur(mask_white, (5,5),0)
+
+                edges = cv2.Canny(blur,50,150)
+                
+                lines = cv2.HoughLines(edges,1,np.pi/180, 150)
+
+                lista_m = []
+                lista_h = []
+
+                linhas_d_m = []
+                linhas_d_x1 = []
+                linhas_d_x2 = []
+                linhas_d_y1 = []
+                linhas_d_y2 = []
+                linhas_d_h = []
+
+                linhas_e_m = []
+                linhas_e_x1 = []
+                linhas_e_x2 = []
+                linhas_e_y1 = []
+                linhas_e_y2 = []
+                linhas_e_h = []
+
+                xis = []
+                yis = []
+                if lines is not None and len(lines) > 0 :
+                    for x in range(0, len(lines)):    
+                        for rho, theta in lines[x]:
+                            a = np.cos(theta)
+                            b = np.sin(theta)
+                            x0 = a*rho
+                            y0 = b*rho
+                            x1 = int(x0 + 1000*(-b))
+                            y1 = int(y0 + 1000*(a))
+                            x2 = int(x0 - 1000*(-b))
+                            y2 = int(y0 - 1000*(a))
+                            m = (y2 - y1)/(x2 - x1)
+                            
+                            h = y1 - m*x1
+
+                            lista_h.append(h)
+                            lista_m.append(m)
+
+                            #direita
+                            if m>0.3 and m<2:
+                                linhas_d_m.append(m)
+                                linhas_d_x1.append(x1)
+                                linhas_d_x2.append(x2)
+                                linhas_d_y1.append(y1)
+                                linhas_d_y2.append(y2)
+                                linhas_d_h.append(h)
+
+
+                            #esquerda
+                            elif m<-0.2 and m>-2:
+                                linhas_e_m.append(m)
+                                linhas_e_x1.append(x1)
+                                linhas_e_x2.append(x2)
+                                linhas_e_y1.append(y1)
+                                linhas_e_y2.append(y2)
+                                linhas_e_h.append(h)
+                            
+                            else:
+                                lista_m.remove(m) 
+                                lista_h.remove(h)
+
+
+            
+        
+                if len(lista_m) > 1 and lista_m[0] != lista_m[1]:
+                    x_i = (lista_h[1] - lista_h[0])/(lista_m[0] - lista_m[1])
+                    y_i = lista_m[0] * x_i + lista_h[0]
+                    x_i = int(x_i)
+                    y_i = int(y_i)
+                    xis.append(x_i)
+                    yis.append(y_i)
+
+            
+                x1 = 0
+                x2 = 0
+                x3 = 0
+                x4 = 0
+                y1 = 0
+                y2 = 0
+                y3 = 0
+                y4 = 0
+                
+                #linha direita
+                if len(linhas_d_m)>1:
+                            x1 = int(np.mean(linhas_d_x1))
+                            x2 = int(np.mean(linhas_d_x2))
+                            y1 = int(np.mean(linhas_d_y1))
+                            y2 = int(np.mean(linhas_d_y2))
+                            cv2.line(frame,(x1,y1), (x2,y2), (50,0,255),2) 
+                
+                #linha esquerda
+                if len(linhas_e_m)>1:
+                            x3 = int(np.mean(linhas_e_x1))
+                            x4 = int(np.mean(linhas_e_x2))
+                            y3 = int(np.mean(linhas_e_y1))
+                            y4 = int(np.mean(linhas_e_y2))
+                            cv2.line(frame,(x3,y3), (x4,y4), (50,0,255),2) 
+
+                #ponto de intersecção
+                if x1!=0 and x2!=0 and x3!=0 and x4!=0:
+                    px = int(((x1*y2 - y1*x2)*(x3-x4) - (x1-x2)*(x3*y4 - y3*x4))/((x1-x2)*(y3-y4) - (y1-y2)*(x3-x4)))
+                    py = int(((x1*y2 - y1*x2)*(y3-y4) - (y1-y2)*(x3*y4-x4*y3))/((x1-x2)*(y3-y4)-(y1-y2)*(x3-x4)))
+                    cv2.circle(frame, (px, py), 1, (0,255,0), 5)
+
+                for r in resultados:
+                    print(r)
+            #velocidade_saida.publish(vel)
             rospy.sleep(0.1)
 
     except rospy.ROSInterruptException:
         print("Ocorreu uma exceção com o rospy")
-
-
